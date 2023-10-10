62e5e7625a679ce1012f04a2e12a830919e8b7d9
Suji299 <147475730+Suji299@users.noreply.github.com>
2023-10-10 11:26:13 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,33 @@
+class Student:
+  def __init__(self,name,roll_number,cgpa):
+    self.name=name
+    self.roll_number=roll_number
+    self.cgpa=cgpa
+
+def sort_students(student_list):
+  sorted_students=sorted(student_list,key=lambda student: student.cgpa,reverse=True)
+  return sorted_students
+
+students=[
+    Student("harish", "A1234",7.8),
+    Student("sachin", "A5678",8.9),
+    Student("sanjana", "A9012",9.1),
+    Student("mani", "A3456",9.9),
+]
+
+sorted_students=sort_students(students)
+
+for student in sorted_students:
+  print("Name:{},Roll number:{},CGPA:{}".format(student.name,student.roll_number,student.cgpa))def linear_search_product(products, target_product):
+  indices = []
+  for i, product in enumerate(products):
+    if product == target_product:
+      indices.append(i)
+  return indices
+
+
+#eg input values
+products = ["apple", "orange", "grapes", "apple", "banana", "papaya", "apple"]
+target_product = "apple"
+result = linear_search_product(products, target_product)
+print(result)
